70a06c065d72c10d27b2f7cc9a77c6e090ac820f
buyaoSPF <taole_2018@163.com>
2023-03-28 15:20:01 +0800

    up

---
+++ app/src/main/java/com/example/sam/chess/test.java
@@ -0,0 +1,11 @@
+package com.example.sam.chess;
+
+public class test {
+
+    public static void main(String[]args) {
+        int a = 1;
+        int b = 2;
+        int result = a+b;
+    }
+    
+}
