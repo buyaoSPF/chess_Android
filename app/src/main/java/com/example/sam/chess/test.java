61702d7c88e68b86a31f4fb50eb70934db394a15
buyaoSPF <taole_2018@163.com>
2023-07-03 15:08:21 +0800

    upupupup

--- app/src/main/java/com/example/sam/chess/test.java
+++ app/src/main/java/com/example/sam/chess/test.java
@@ -2,6 +2,8 @@ package com.example.sam.chess;
 
 public class test {
 
+	public Integer a = 2;
+
     public static void main(String[]args) {
         int a = 1;
         int b = 2;
@@ -9,6 +11,7 @@ public class test {
         int test = 2023421;
         int time = 1610;
         System.out.println("upupup");
+        System.out.println("fucku!");
 
         int result = a+b;
 
